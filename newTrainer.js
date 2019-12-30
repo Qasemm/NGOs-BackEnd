5b4f87f89bdfd4493da225fc7c72b8502c02e901
ahmadammmoura <55942495+ahmadammmoura@users.noreply.github.com>
2019-12-30 03:22:52 -0800

    alia push

--- newTrainer.js
+++ newTrainer.js
@@ -10,49 +10,48 @@ function changeImage(input) {
    reader.onload = function(e) {
      preview.setAttribute('src', e.target.result);
    }
- reader.readAsDataURL(input.files[0]);
+   reader.readAsDataURL(input.files[0]);
  }}
  document.getElementById('save').addEventListener('click',add);
-////////////////////////////////////
- 
-input   = document.getElementById("filetag");
 
+/////////
+
+
+input   = document.getElementById("filetag");
+let b64 = "";
 
 input.onchange = function () {
+
   var file = input.files[0],
     img = new FileReader();
-    
+
 
   img.onloadend = function () {
-    var b64 = img.result.replace(/^data:.+;base64,/, '');
+    b64 = img.result.replace(/^data:.+;base64,/, '');
 
-    console.log(b64); 
+    
   };
 
   img.readAsDataURL(file);
 };
 
+///
+x = "bashar ahmad ammoura"
+btoa(x)
+
+console.log(btoa(x))
 
 
-
-
-/////////////
+/////
 function add(){
+  console.log(b64); 
 name    = document.getElementById("name").value;
 email   = document.getElementById("email").value;
 num     = document.getElementById("num").value;
 address = document.getElementById("address").value;
-photo   = document.getElementById("filetag");
+photo   = document.getElementById("filetag").value
 
-
-
-
-
-
-
-
-
-if (name  == "" ){
+if (name === "" ){
   alert("add some information")
 }else{
 
@@ -68,7 +67,7 @@ if (name  == "" ){
           email : email,
           num : num ,
           address : address,
-          photo : b64
+          // photo : b64
         })
 
 })
