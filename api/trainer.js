38fc64f34ef00baa8e7e58b9cbfc7f065273fefa
maramabdulla <maram.alnaimi95@gmail.com>
2020-01-13 04:27:45 -0800

    improve project structure

---
+++ api/trainer.js
@@ -0,0 +1,21 @@
+function getAllTrainers(token, callback) {
+    // fetch('http://localhost:3000/trainer' , {
+    //     method: 'GET',
+    // })
+    // .then(Response =>
+    //     Response.json()
+    // ).then( data =>{ 
+    //     if (callback) {
+    //         callback(data)
+    //     }
+    // })
+
+    callback([
+        {
+            id: 1,
+            
+        },
+        {},
+        {},
+    ]);
+}
