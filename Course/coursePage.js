f97fdacf95d293bd559a0c5f3d1d26bd659a719d
maramabdulla <maram.alnaimi95@gmail.com>
2020-01-21 23:59:00 -0800

    frontEnd with backEnd

--- Course/coursePage.js
+++ Course/coursePage.js
@@ -11,15 +11,28 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 getInfOneCourse();
+// let save_btn = document.getElementById('save');
+// save_btn.style.display = "none";
+// let ProfileImg = document.getElementById('preview').style.cursor = "pointer";
+// let cancle_btn = document.getElementById('cancle');
+// cancle_btn.style.display = "none";
+// let token = localStorage.getItem('token');
+// let header = new Headers();
+// header.append('content-type', 'application/json')
+// header.append('authorization', token)
+
+
 ////////////////////////////////////////////////
 function renderOneCourses(data) {
 
     const htmlArray = data.map(( courses,index) =>
-    ' <div> <h3 >title :</h3> <p class="para">'+courses.title+'</p></div> <div>  <h3 >Trainer Of Course:</h3>  <a href="#" class="para aLink">'+courses.trainer+'</a> </div>     <div> <h3>descrption : </h3> <p class="para">'+courses.description+
-    '</p> </div>  <div><h3>dates :</h3> <p class="para">'+ courses.start_date+ '    To   '+ courses.end_datel+
-    '</p></div> <div> <h3> location :</h3><p class="para">'+courses.location+
-    ' </p> </div><div> <h3>number of seats:    </h3> <p class="para">'+courses.number_of_seats+
-    '</p> </div><div> <h3> Trainee Of Course:</h3><a id="traineeLink"   class="para aLink" onclick="getListTrainee()"> Trainee</a> </div> '
+    ' <div> <h3 >title :</h3> <input id="title" type="text" class="para input" value="'+courses.title+
+    '"></input></div> <div>  <h3 >Trainer Of Course:</h3>  <a href="#" class="para aLink" onclick="getPorfileTrainer()">'+courses.trainer+
+    '</a> </div>  <div> <h3>descrption : </h3> <input  id="description"type="text" class="para input" value="'+courses.description+
+    '"></input></div>  <div><h3>dates :</h3> <input id="dates1" type="text" class="para input" value="'+ courses.start_date+'"></input> </div> <div><h3>dates :</h3>   <input id="dates2" type="text" class="para input" value="'+ courses.end_datel+
+    '"></input></div> <div> <h3> location :</h3><input id="location" type="text"class="para input" value="'+courses.location+
+    '"> </input> </div><div> <h3>number of seats:    </h3> <input id="number" type="text" class="para input" value="'+courses.number_of_seats+
+    '"></input> </div><div> <h3> Trainee Of Course:</h3><a id="traineeLink"   class="para aLink" onclick="getListTrainee()"> Trainee</a> </div>  <div class="btn">  <button type="submit" id="save" class="savebtn" onclick="change_value()">Save</button>  <button type="submit" id="cancle" class="Canclebtn" onclick="goBack()">Cancle</button></div> '
     );
     // console.log(htmlArray);
     document.getElementById("container").innerHTML = htmlArray;
@@ -45,7 +58,7 @@ function renderOneCourses(data) {
   ///////////////////////////////////////////////////////////////
   // document.getElementById("traineeLink").addEventListener('click',getListTrainee);
   function getListTrainee(){
-    // window.location = "../Course/coursePage.html";
+    window.location = "../Trainee/getAllTrainee.html";
     let id=getParameterByName("id", url);
     console.log(id);
     fetch('http://localhost:3000/courses/trainee/'+id,
@@ -62,10 +75,58 @@ function renderOneCourses(data) {
           trainer => '<div class="list"><div  class="im"><img onclick="openProfile()" id = ' + trainer.id + ' src="" alt=""></div><div  class="im contnt desc"><h3 >' + trainer.name + ' <img class="remove" id = ' + trainer.id + '  src="img/error.png"></h3><p>' + trainer.email + '</p><p >' + trainer.mobile + '</p><p>' + trainer.address + '</p></div></div></a></div>'
         );
 
-        document.getElementById("container1").innerHTML += htmlArray.join('');
+        document.getElementById("div1").innerHTML += htmlArray.join('');
              
         };
         renderTrainees(data);
     })
     
   }
+  //////////////////////////////////////////////////////////////
+function getPorfileTrainer(){
+  
+}
+  //////////////////////////////////////////////////////////////
+
+function change_value() {
+  let CourseName=document.getElementById("title");
+let DateBegin=document.getElementById("dates1");
+let DateEnd=document.getElementById("dates2");
+let CourseLocation=document.getElementById("location");
+let NumberOfSeats=document.getElementById("number"); 
+const desc=document.getElementById('description');
+// let trainerName = document.getElementById("addtriner").value;
+// console.log(trainerName);
+
+
+let header=new Headers();
+header.append("content-type", "application/json");
+let id=getParameterByName("id", url);
+console.log(id);
+fetch ('http://localhost:3000/courses/'+id,{
+method: 'PUT',
+headers:header,
+body:JSON.stringify({
+    title:CourseName.value,
+    location:CourseLocation.value,
+    desctiption:desc.value,
+    date_begin:DateBegin.value,
+    date_end:DateEnd.value,
+    number_of_seats:NumberOfSeats.value
+})
+    }   
+    ).then(res=>{
+        res.json()
+    })
+    .then(data=>{
+        console.log(data);
+        
+        
+    });
+
+  }
+  let cancle_btn=document.getElementById('cancle');
+  // cancle_btn.addEventListener('click',goBack);
+ function goBack() {
+window.location = "../Course/index.html"; //specify the url to redirect
+}
